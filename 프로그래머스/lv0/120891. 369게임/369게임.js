52bf04850c999f539b5aff8c6148df4ffee675de
JiHun-yam <95469708+JiHun-yam@users.noreply.github.com>
2023-01-25 23:21:54 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,15 +1,7 @@
 function solution(order) {
-    let 배열s = [...(""+order)] //2,9,4,2,3
-    
-    삼육구 = 0
-    for (i=0; i < 배열s.length; i++){
-        if (배열s[i] == 3 || 배열s[i] == 6 ||배열s[i] == 9 && 배열s[i] !== 0)  {
-            삼육구 ++ 
-        } 
-      
-        
-    }
-    
-    return 삼육구;
+    var answer = 0;
+    let order1 =  String(order)
+    console.log(order1)
 }
+    
   
