56178247a50fcd3a82657fc745c410d1ccf5bc98
JiHun-yam <95469708+JiHun-yam@users.noreply.github.com>
2023-01-27 20:54:54 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,9 +1,12 @@
 function solution(a, b) {
+    //0으로 
     var answer = 0;
-
-     
+    //반복문으로 돌려서 
     for(let i =0; i< a.length; i++){
+        //a[0] * a[0] ~~~...~~~ a[마지막인덱스] * b[마지막인덱스]
+        // 그걸 answer  += 해서 넣고
         answer += a[i]*b[i];
     }
+    //출력 하면 끝~~~~~~~~
     return answer;
 }
