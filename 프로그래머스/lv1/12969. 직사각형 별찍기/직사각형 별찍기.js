86788583167ca5f5910e91323964176ff99f71d6
JiHun-yam <95469708+JiHun-yam@users.noreply.github.com>
2023-01-27 10:15:22 +0900

    [level 1] Title: 직사각형 별찍기, Time: 69.36 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,19 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  // n[0] == a b = n = n[1]
+  // a == 한줄에 몇개찍을지 b == 몇줄을 반복할지
+  const a = Number(n[0]),
+    b = Number(n[1]);
+
+  for (let i = 0; i < b; i++) {
+    let stat1 = "";
+    // b가 길이
+    for (let j = 0; j < a; j++) {
+      //a 한줄에 갯수
+      stat1 += "*";
+    }
+    console.log(stat1);
+  }
+
+});
