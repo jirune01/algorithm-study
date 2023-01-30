d475bbee161b9565ceb217d84d2951501fe9d6de
JiHun-yam <95469708+JiHun-yam@users.noreply.github.com>
2023-01-30 16:24:24 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,7 +1,13 @@
+function solution(s) {
+    var answer = '';
+    let arr = s.split('')
+    console.log(arr)
+    return answer;
+}
 
-  function solution(s) {
-     
-      return s.split('').sort().reverse().join('')
-
+// function solution(s) {
+//     let ss = s.split('').reverse().join('')
+//     return ss.toString('')
+// //     s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
    
-  }
+// }
