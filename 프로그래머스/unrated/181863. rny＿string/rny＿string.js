262edaca446da985c7ec43b8ad047c721eb673ed
Jihun <95469708+JiHun-yam@users.noreply.github.com>
2023-05-24 20:24:32 +0900

    [unrated] Title: rny_string, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.js
@@ -0,0 +1,11 @@
+function solution(rny_string) {
+    var answer = [];
+     for(let i =0; i<rny_string.length; i++){
+        if(rny_string[i] == 'm'){
+            answer.push('rn')
+        }else{
+            answer.push(rny_string[i])
+        }
+     }
+    return answer.join('');
+}
