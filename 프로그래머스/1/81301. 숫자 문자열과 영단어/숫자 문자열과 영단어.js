e22478a3677b090162760196120a3ab19f8dc83b
Jihun <95469708+Jihun1215@users.noreply.github.com>
2023-11-25 23:35:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  let nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let answer = s;
+
+    for(let i = 0; i < nums.length; i++) {
+        let arr = answer.split(nums[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
