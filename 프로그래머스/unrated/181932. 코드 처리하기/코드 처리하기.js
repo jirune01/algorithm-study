083f95e588d9798a9784dec1705905d2f13aff4e
Jihun <95469708+Jihun1215@users.noreply.github.com>
2023-12-01 22:35:15 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,12 @@
+function solution(code) {
+    let mode = 0;
+    let ret = '';
+
+    for(let i=0; i<code?.length; i++){
+        if(mode === 0) code[i] == "1" ? mode =1 : i%2==0&&(ret+=code[i])
+        else code[i] =='1' ? mode=0:i%2!==0&&(ret+=code[i])
+    }
+
+    return ret.length>0? ret:"EMPTY";
+
+}
