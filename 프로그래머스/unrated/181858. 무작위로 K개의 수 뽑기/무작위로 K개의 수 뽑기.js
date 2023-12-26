22d748c69523edb2a2ca4dff69b38dad3714dc2c
Jihun <95469708+Jihun1215@users.noreply.github.com>
2023-12-26 23:05:12 +0900

    [level unrated] Title: 무작위로 K개의 수 뽑기, Time: 0.77 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,14 @@
+function solution(arr, k) {
+    let answer = [];
+
+    for(let i=0;i<arr.length;i++){
+        if(answer.length === k) return answer;
+        if(answer.indexOf(arr[i]) === -1) answer.push(arr[i]);   
+    }
+
+    while(answer.length < k){
+        answer.push(-1);
+    }
+
+    return answer;
+}
