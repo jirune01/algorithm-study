6956e562541e76885a8d1c97ab6b8c9e61dec8d5
Jihun <95469708+Jihun1215@users.noreply.github.com>
2024-01-19 23:38:40 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,15 @@
+function solution(brown, yellow) {
+    let answer = [];
+    let sum = brown + yellow;
+    
+
+    for(let i=3; i<=brown; i++){
+        if(sum % i === 0){
+            let weight = sum / i;
+            if( (i-2) * (weight-2) === yellow){
+                return [weight, i];
+            }
+        }
+    }
+    return answer;
+}
