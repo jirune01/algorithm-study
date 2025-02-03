be0a01cda2b0e3d01898f97120d03847e11f6187
jirune <95469708+jirune01@users.noreply.github.com>
2025-02-03 21:23:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,9 @@
+function solution(x) {
+    let answer = 0;
+    let arr = x.toString().split('')
+    for(let i = 0; i < arr.length; i++){
+      answer += Number(arr[i])
+    }
+    return (x % answer === 0) ? true : false ;
+  
+}
