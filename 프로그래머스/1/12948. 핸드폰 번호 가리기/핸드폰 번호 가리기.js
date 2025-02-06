0e7dbeda7d94ed6a0695b290ea248fd1d2500803
jirune <95469708+jirune01@users.noreply.github.com>
2025-02-06 23:27:28 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+    let answer = '';
+    // 뒷 자리 4개 제외한 번호들 
+     const len = phone_number.length - 4;
+    // 앞자리 * 표시 하고 뒷 4자리를...
+    return "*".repeat(len) + phone_number.substring(len)
+}
