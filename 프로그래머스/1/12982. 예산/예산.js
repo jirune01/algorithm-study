1e539166674fc188c8aaa37e1723f1a4540a4bc3
jirune <95469708+jirune01@users.noreply.github.com>
2025-02-23 21:11:13 +0900

    [level 1] Title: 예산, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,13 +1,14 @@
 function solution(d, budget) {
- var answer = 0, sum = 0;
+    let answer = 0, sum = 0;
     d.sort((a,b) => a - b);
 
     for(let i = 0; i < d.length; i++){
         answer++;
         sum += d[i]
 
-        if(sum > budget)
-            answer--;
+        if(sum > budget) {
+          answer--;
+        }
     }
   
     return answer;
