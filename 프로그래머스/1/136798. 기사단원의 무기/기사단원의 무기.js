9fc7b47b923a27901be706d724f649d8a659c8ec
jirune <95469708+jirune01@users.noreply.github.com>
2025-03-09 22:58:01 +0900

    [level 1] Title: 기사단원의 무기, Time: 98.58 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -3,16 +3,20 @@ function solution(number, limit, power) {
 
   for (let i = 1; i <= number; i++) {
     let divisor = 0;
+
     for (let j = 1; j <= Math.sqrt(i); j++) {
+
       if (i % j === 0) {
         if (i / j === j) divisor += 1;
         else divisor += 2;
       }
+
       if (divisor > limit) {
         divisor = power;
         break;
       }
     }
+
     answer += divisor;
   }
   
