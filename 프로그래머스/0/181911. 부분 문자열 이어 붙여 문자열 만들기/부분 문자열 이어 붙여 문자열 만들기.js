857be3f4d607ebebd1d28e1053d177d24458d403
jirune <95469708+jirune01@users.noreply.github.com>
2025-03-11 00:22:49 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.46 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(my_strings, parts) {
+    let answer = '';
+
+    for(let i = 0; i< my_strings.length; i++){
+      answer = answer + my_strings[i].split('').slice(parts[i][0] ,parts[i][1]+1).join('')
+    }
+
+    return answer;
+}
