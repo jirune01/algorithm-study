26cd831f2515d219ed3f1217f61236ddb9a7acf0
음지훈 (Jihun Eum) <95469708+jirune01@users.noreply.github.com>
2025-11-12 09:06:33 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.31 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,14 +1,13 @@
 function solution(k, score) {
-    let answer = [];
+    let result = [];
     let nums = [];
     
-    score.forEach(num => {
-        nums.push(num);
+    score.forEach(item => {
+        nums.push(item);
+        nums.sort((a,b) => b - a).splice(k);
         
-        nums.sort((a,b) => b-a).splice(k);
-        
-        answer.push(Math.min.apply(null, nums));
+        result.push(Math.min.apply(null, nums));
     });
 
-    return answer;
+    return result;
 }
