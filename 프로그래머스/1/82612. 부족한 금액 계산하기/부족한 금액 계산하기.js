501811841154b4d61a812b09aeb7e51bcab634ec
음지훈 (Jihun Eum) <95469708+jirune01@users.noreply.github.com>
2025-11-13 09:05:30 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,17 +1,16 @@
 function solution(price, money, count) {
+    
     let totalMoney = 0;
 
     for (let i = 1; i <= count; i++) {
         totalMoney += price * i;
     }
 
-    if(totalMoney > money) {
-      return totalMoney - money
+    if(totalMoney > money){
+        return totalMoney - money
     } else if(totalMoney <= money) {
         totalMoney = 0;
         return totalMoney
     }
-    
-
     
 }
