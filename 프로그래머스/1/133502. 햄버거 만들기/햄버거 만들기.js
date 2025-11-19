d45e0c0e6ff6adaa4819ce94ebd5645c51569449
음지훈 (Jihun Eum) <95469708+jirune01@users.noreply.github.com>
2025-11-19 09:07:29 +0900

    [level 1] Title: 햄버거 만들기, Time: 7177.12 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,20 +1,15 @@
 function solution(ingredient) {
-    let answer = 0;
-    const stack = [];
+    let burger = 0;
     
-    for (let i = 0; i < ingredient.length; i++) {
-        stack.push(ingredient[i]);
-        
-        if (stack.length >= 4) {
-            const burger_ingredient = stack.slice(-4).join("");
-            
-            if (burger_ingredient === '1231') {
-                stack.splice(-4);
-                answer += 1;
-            }
+    for(let i = 0; i < ingredient.length; i++) {
+        // 빵 야채 고기 빵 일떄
+        if(ingredient.slice(i, i + 4).join('') === '1231') {
+            burger++;
+            ingredient.splice(i, 4)
+            i -= 3
         }
-
     }
-    
-    return answer;
+    return burger
 }
+
+
