29d604bd2c32171fbbed779ed9357bcb0f0fa76f
음지훈 (Jihun Eum) <95469708+jirune01@users.noreply.github.com>
2025-12-15 09:02:03 +0900

    [level 1] Title: 내적, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,3 @@
 function solution(a, b) {
-    let answer = 0;
-    for(let i = 0; i < a.length; i++){
-        answer += a[i] * b[i];
-    }
-    return answer;
+     return a.map((v,i,arr) => v*b[i]).reduce((ac,cr) => ac + cr ,0);
 }
