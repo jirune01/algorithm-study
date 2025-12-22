9840d4a55a8fe9aa33c33cf156f546377c8549ed
음지훈 (Jihun Eum) <95469708+jirune01@users.noreply.github.com>
2025-12-22 09:16:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 115.43 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+    participant.sort()
+    completion.sort()
+    while (participant.length) {
+        let pp = participant.pop()
+        if (pp !== completion.pop()) {
+            return pp
+        }
+    }
+}
